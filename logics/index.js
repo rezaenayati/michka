16a4774949d1dbb68b38a14fd93499bfa42d13f7
Reza Enayati <enayatii.reza@gmail.com>
2019-02-27 00:17:21 +0330

    show name in LinksScreen

--- logics/index.js
+++ logics/index.js
@@ -1,21 +1,29 @@
-export const fetchUser = (username) => {
-  var data = null;
 
+export const fetchUser = (username) => {
+  var data = {};
   var xhr = new XMLHttpRequest();
   xhr.withCredentials = true;
 
-  xhr.addEventListener("readystatechange", function () {
-    if (this.readyState === 4) {
-      console.log(this.responseText);
-    }
-  });
-
+  xhr.responseType = 'json';
   xhr.open("GET", "https://api.twitter.com/1.1/users/show.json?user_id=870812911119347712");
   xhr.setRequestHeader("Authorization", "OAuth oauth_consumer_key=\"ziUG9xZPCKMAH9ggYfrbLRiQE\",oauth_token=\"870812911119347712-tbY6QIJJqMymC58RA1ifINm0MEFYnXn\",oauth_signature_method=\"HMAC-SHA1\",oauth_timestamp=\"1551170182\",oauth_nonce=\"rOTwEAb3A3L\",oauth_version=\"1.0\",oauth_signature=\"xYMMd%2FEU4ZLVbYwtZwZfUK19%2BKs%3D\"");
   xhr.setRequestHeader("cache-control", "no-cache");
-  xhr.setRequestHeader("Postman-Token", "57bbc284-b2d4-4159-8d6b-4f29dee8ba4d");
 
+  xhr.onload  = () => {
+     var jsonResponse = xhr.response;
+     // console.log(jsonResponse);
+     data = jsonResponse;
+     console.log(data.id);
+     return jsonResponse;
+  };
   xhr.send(data);
-  // console.log("In There!");
-  console.log(data);
+  console.log("In There!");
 };
+export const test = () => {
+  return new Promise((resolve , reject) => {
+    setTimeout(() => {
+      // console.log("How you doing??");
+      // console.log(data);
+    }, 5000);
+  });
+}
--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import { ScrollView, StyleSheet, Text } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
-import {fetchUser} from '../logics';
-
-const importantData = null;
-
+import {fetchUser , test} from '../logics';
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
@@ -15,20 +12,32 @@ export default class LinksScreen extends React.Component {
     data: null,
   };
   componentWillMount(){
-    fetchUser();
+    this.fetchUser2();
   }
 
   componentDidMount() {
-    console.log("A log full of love!");
-    temp = "Hello World!!";
-    // fetchUser();
-    fetchUser();
-    console.log(temp);
-    this.setState({data : temp});
-    // console.log(fetchUser2());
+    setTimeout(() => {
+      console.log("Hey Joey do wanna eat some juice?");
+    }, 5000);
   }
 
+  fetchUser2 = (username) => {
+    var inf = {};
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
 
+    xhr.responseType = 'json';
+    xhr.open("GET", "https://api.twitter.com/1.1/users/show.json?user_id=870812911119347712");
+    xhr.setRequestHeader("Authorization", "OAuth oauth_consumer_key=\"ziUG9xZPCKMAH9ggYfrbLRiQE\",oauth_token=\"870812911119347712-tbY6QIJJqMymC58RA1ifINm0MEFYnXn\",oauth_signature_method=\"HMAC-SHA1\",oauth_timestamp=\"1551170182\",oauth_nonce=\"rOTwEAb3A3L\",oauth_version=\"1.0\",oauth_signature=\"xYMMd%2FEU4ZLVbYwtZwZfUK19%2BKs%3D\"");
+    xhr.setRequestHeader("cache-control", "no-cache");
+
+    xhr.onload  = () => {
+       var jsonResponse = xhr.response;
+       this.setState({data : jsonResponse.name});
+    };
+    xhr.send(inf);
+    console.log("In There!");
+  };
 
   render() {
     return (
